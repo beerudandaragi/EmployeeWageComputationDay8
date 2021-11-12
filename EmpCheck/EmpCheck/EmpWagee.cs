21a9815203a27e94ab1c2baef1576eea18be455d
beerappa <beerappadandaragi@gmail.com>
2021-11-12 15:03:34 +0530

    SwitchCase

---
+++ EmpCheck/EmpCheck/EmpWagee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpCheck
+{
+    class EmpWagee
+    {
+        public void compute()
+        {
+            const int Is_Part_Time = 1;
+            const int IS_FULLTIME = 2;
+            const int EMP_RATE_PER_HOURS = 300;
+            int EmpHours = 0;
+            int EmpDailyWage = 0;
+
+            Random rand = new Random();
+
+            int randomvalue = rand.Next(0, 3);
+
+            switch (randomvalue)
+            {
+                case IS_FULLTIME:
+                    EmpHours = 8;
+
+                    break;
+                case Is_Part_Time:
+                    EmpHours = 4;
+
+                    break;
+                default:
+                    EmpHours = 0;
+
+                    break;
+            }
+            EmpDailyWage = EmpHours * EMP_RATE_PER_HOURS;
+            Console.WriteLine("Employee Wage is " + EmpDailyWage);
+            Console.ReadLine();
+        }
+    }
+}
+    
+
--- EmpCheck/EmpCheck/Program.cs
+++ EmpCheck/EmpCheck/Program.cs
@@ -2,48 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace EmpWagePartTime
+namespace EmpCheck
 {
-    public class EmpPartTime
+    class program
     {
-        public void compute()
+        public static void Main(string[] args)
         {
-            int Is_Part_Time = 1;
-            int IS_FULLTIME = 2;
-
-            int EmpWagePerHour = 200;
-            int EmpHours = 0;
-            int EmpDailyWage = 0;
-
-            Random rand = new Random();
-            int randomvalue = rand.Next(0, 3);
-            if (randomvalue == IS_FULLTIME)
-            {
-                EmpHours = 8;
-
-            }
-            else if (randomvalue == Is_Part_Time)
-            {
-                EmpHours = 4;
-
-            }
-            else
-            {
-                EmpHours = 0;
-
-            }
-            EmpDailyWage = EmpHours * EmpWagePerHour;
-            Console.WriteLine("Employee Wage is " + EmpDailyWage);
-            Console.ReadKey();
-        }
-        class Program
-        {
-            public static void Main(string[] args)
-            {
-                EmpPartTime EmpObj1 = new EmpPartTime();
-                EmpObj1.compute();
-
-            }
+            EmpWagee EmpWageeobj1 = new EmpWagee();
+            EmpWageeobj1.compute();
         }
     }
 }
+    
