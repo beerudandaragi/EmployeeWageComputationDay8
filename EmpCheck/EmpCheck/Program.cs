fde00d092bc8966e54684f3a082debb65906670d
beerappa <beerappadandaragi@gmail.com>
2021-11-12 14:36:13 +0530

    Welcome To Employee Wage computation

---
+++ EmpCheck/EmpCheck/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePresentOrAbsent
+{
+    class empAttendance
+    {
+        public void compute()
+        {
+
+            const int Present = 1;
+            const int Absent = 0;
+
+            Random rand = new Random();
+            int check = rand.Next(0, 2);
+            if (check == Present)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else if (check == Absent)
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadKey();
+        }
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                empAttendance obj1 = new empAttendance();
+                obj1.compute();
+
+            }
+        }
+    }
+}
